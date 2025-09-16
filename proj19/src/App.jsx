f03ad73b28b60d60b4ebb29cbe423f283afd5605
abhibs <abhiram.bs@webdigitalmantra.in>
2025-09-17 00:34:22 +0530

    managing click button

---
+++ proj19/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <button onClick={alert('Clicked')}>Click Me</button>
+        </div>
+    );
+};
+
+export default App;
