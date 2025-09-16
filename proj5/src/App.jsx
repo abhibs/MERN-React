cfd927bc667a34368e1342b439b474d2f40dff82
abhibs <abhirambs97@gmail.com>
2025-09-16 16:37:31 +0530

    multiple component created

---
+++ proj5/src/App.jsx
@@ -0,0 +1,17 @@
+import Abhi from './components/Abhi'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Abhi />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ proj5/src/components/Abhi.jsx
@@ -0,0 +1,9 @@
+const Abhi = () => {
+  return (
+    <div>
+      <h1>Abhiram B S Javalli Tudoor Thirthahalli Shimoga Karnataka 577226</h1>
+    </div>
+  )
+}
+
+export default Abhi
