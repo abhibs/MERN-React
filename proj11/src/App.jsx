3d6907b019982b64a55e0c543388b5641ede8c73
abhibs <abhiram.bs@webdigitalmantra.in>
2025-09-16 21:46:05 +0530

    using switch case conditional rendering

---
+++ proj11/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const status = true
+  switch(status){
+    case true:
+      return <button>Logout</button>
+    case false:
+      return <button>Login</button>
+    default:
+      return null
+  }
+};
+
+export default App;
