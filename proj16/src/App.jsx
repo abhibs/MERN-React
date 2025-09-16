05cd837e1900fccfef108b41a8c1068549e928f0
abhibs <abhiram.bs@webdigitalmantra.in>
2025-09-16 23:38:27 +0530

    passing data parent component to child component using props

---
+++ proj16/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Abhi from './components/Abhi';
+
+const App = () => {
+  return (
+    <div>
+       <Abhi name="Abhiram B S" location="Javalli Tudoor Thirthahalli Shimoga Karnataka 577226"/>
+    </div>
+  );
+};
+
+export default App;
---
+++ proj16/src/components/Abhi.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Abhi = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <p>{props.location}</p>
+        </div>
+    );
+};
+
+export default Abhi;
