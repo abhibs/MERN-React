ba6056b00b43806323440c2e77a5a388b2d8ae9f
abhibs <abhirambs97@gmail.com>
2025-09-17 14:27:30 +0530

    create nav link to  route using menu component

--- proj21/src/components/Menu.jsx
+++ proj21/src/components/Menu.jsx
@@ -1,17 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Menu = () => {
   return (
     <div>
       <ul>
-        <li>
+        {/* <li>
           <Link to='/'>Home</Link>
-        </li>
+        </li> */}
+
         <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        {/* <li>
           <Link to='/about'>About</Link>
-        </li>
+        </li> */}
         <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
+
+        {/* <li>
           <Link to='/contact'>Contact</Link>
+        </li> */}
+        <li>
+          <NavLink to='/contact'>Contact</NavLink>
         </li>
       </ul>
     </div>
