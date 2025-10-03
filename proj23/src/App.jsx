3358e6d6471215d516f960a851b5c1f29b790c54
abhibs <abhirambs97@gmail.com>
2025-10-03 18:14:10 +0530

    use ref working with img attributes example

--- proj23/src/App.jsx
+++ proj23/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const App = () => {
+  let myImg = useRef()
+
+  const change = () => {
+    myImg.current.src = 'https://placehold.co/600x400/000000/ffffff'
+  }
   return (
     <div>
-      <img src='https://placehold.co/600x400' alt='' />
+      <img ref={myImg} src='https://placehold.co/600x400' alt='' />
       <br />
       <br />
       <br />
-      <button>Change</button>
+      <button onClick={change}>Change</button>
     </div>
   )
 }
