4c148c843de288a67c81d0c520bb79b774268244
abhibs <abhirambs97@gmail.com>
2025-10-03 18:04:57 +0530

    image and one button added

---
+++ proj23/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <img src='https://placehold.co/600x400' alt='' />
+      <br />
+      <br />
+      <br />
+      <button>Change</button>
+    </div>
+  )
+}
+
+export default App
