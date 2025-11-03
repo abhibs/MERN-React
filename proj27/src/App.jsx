7c5839643a177183fbd0a9d61fb040a5ac452f7e
abhibs <abhirambs97@gmail.com>
2025-11-03 15:59:36 +0530

    useState working with numbers with seperate function

--- proj27/src/App.jsx
+++ proj27/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  let [number, setNumber] = useState(1)
+  const [number, setNumber] = useState(0)
 
+  const changeNumber = () => {
+    setNumber(number - 1)
+  }
   return (
     <div>
       <h1>Number: {number}</h1>
-      <button onClick={() => setNumber(number + 1)}>Click</button>
+      <button onClick={changeNumber}>Click</button>
     </div>
   )
 }
