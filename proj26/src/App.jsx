62c690fd8d43edd4a4e7446a91fc2f006c70047f
abhibs <abhirambs97@gmail.com>
2025-11-03 15:17:54 +0530

    show dummy api data

--- proj26/src/App.jsx
+++ proj26/src/App.jsx
@@ -2,15 +2,22 @@ import React, { useRef } from 'react'
 
 const App = () => {
   let apiData = useRef(null)
-
+  let myData = useRef()
   const fetchData = async () => {
     const response = await fetch('https://dummyjson.com/products')
     apiData.current = await response.json()
   }
 
+  const showData = () => {
+    myData.current.innerText = JSON.stringify(apiData.current)
+  }
+
   return (
     <div>
+      <p ref={myData}></p>
       <button onClick={fetchData}>Fetch API</button>
+      <br />
+      <button onClick={showData}>Show Data</button>
     </div>
   )
 }
