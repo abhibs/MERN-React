238342023613c28f91a0059303b8dd01d5f8a76e
abhibs <abhirambs97@gmail.com>
2025-11-04 13:07:52 +0530

    useState working with immutable object with update object

--- proj28/src/App.jsx
+++ proj28/src/App.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [myObj] = useState({
+  const [myObj, setMyObj] = useState({
     key1: 'Abhi 1',
     key2: 'Abhi 2',
     key3: 'Abhi 3',
   })
+
+  const changeObj = () => {
+    setMyObj({
+      key1: 'Abhiram B S 1',
+      key2: 'Abhiram B S 2',
+      key3: 'Abhiram B S 3',
+    })
+  }
 
   return (
     <div>
       <h1>Number: {myObj.key1}</h1>
       <h1>Number: {myObj.key2}</h1>
       <h1>Number: {myObj.key3}</h1>
-      <button onClick=''>Click</button>
+      <button onClick={changeObj}>Click</button>
     </div>
   )
 }
