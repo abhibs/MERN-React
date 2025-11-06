a673d6581e9e55328456c1a884fbd493f890d4bc
abhibs <abhirambs97@gmail.com>
2025-11-06 18:11:29 +0530

    form submit shows in console

--- proj30/src/App.jsx
+++ proj30/src/App.jsx
@@ -1,26 +1,66 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  let [formObj] = useState({
+  let [formObj, setFormObj] = useState({
     fname: '',
     lname: '',
     city: '',
     gender: '',
   })
+
+  const inputOnChange = (property, value) => {
+    setFormObj((prevObj) => ({
+      ...prevObj,
+      [property]: value,
+    }))
+  }
+
+  const FormSubmit = (e) => {
+    e.preventDefault()
+    console.log(formObj)
+  }
+
   return (
     <div className='container'>
-      <form action=''>
-        <input value={formObj.fname} placeholder='First Name' />
-        <input value={formObj.lname} placeholder='Last Name' />
-        <select value={formObj.city}>
+      <form onSubmit={FormSubmit}>
+        <input
+          onChange={(e) => {
+            inputOnChange('fname', e.target.value)
+          }}
+          value={formObj.fname}
+          placeholder='First Name'
+        />
+        <input
+          onChange={(e) => {
+            inputOnChange('lname', e.target.value)
+          }}
+          value={formObj.lname}
+          placeholder='Last Name'
+        />
+        <select
+          onChange={(e) => {
+            inputOnChange('city', e.target.value)
+          }}
+          value={formObj.city}
+        >
           <option value=''>Choose City</option>
           <option value='Banglore'>Banglore</option>
           <option value='Shimoga'>Shimoga</option>
           <option value='Shimoga'>Ugser</option>
         </select>
-        <input type='radio' checked={formObj.gender === 'Male'} name='gender' />
+        <input
+          onChange={() => {
+            inputOnChange('gender', 'Male')
+          }}
+          type='radio'
+          checked={formObj.gender === 'Male'}
+          name='gender'
+        />
         Male
         <input
+          onChange={() => {
+            inputOnChange('gender', 'Female')
+          }}
           checked={formObj.gender === 'Female'}
           type='radio'
           name='gender'
