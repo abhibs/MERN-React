aa303fbc564c2f33989354b6f5d63a318c76b4c1
abhibs <abhirambs97@gmail.com>
2025-11-06 17:12:37 +0530

    display the value

--- proj30/src/App.jsx
+++ proj30/src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  let [formObj] = useState({
+    fname: '',
+    lname: '',
+    city: '',
+    gender: '',
+  })
   return (
     <div className='container'>
       <form action=''>
-        <input placeholder='First Name' />
-        <input placeholder='Last Name' />
-        <select>
+        <input value={formObj.fname} placeholder='First Name' />
+        <input value={formObj.lname} placeholder='Last Name' />
+        <select value={formObj.city}>
           <option value=''>Choose City</option>
           <option value='Banglore'>Banglore</option>
           <option value='Shimoga'>Shimoga</option>
           <option value='Shimoga'>Ugser</option>
         </select>
-        <input type='radio' name='gender' />
+        <input type='radio' checked={formObj.gender === 'Male'} name='gender' />
         Male
-        <input type='radio' name='gender' />
+        <input
+          checked={formObj.gender === 'Female'}
+          type='radio'
+          name='gender'
+        />
         Female
         <br />
         <button type='submit'>Submit</button>
